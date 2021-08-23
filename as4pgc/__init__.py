f9826999070bf9e056792137e6a2b142893147b3
ClarkFieseln <54716944+ClarkFieseln@users.noreply.github.com>
2021-08-23 08:12:47 +0200

    default configuration synched with config.ini

--- as4pgc/__init__.py
+++ as4pgc/__init__.py
@@ -1,4 +1,4 @@
 # Version of Module
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 # This call to setup() does all the work
 setup(
