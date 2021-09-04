74fc058d19255bd02d8a41a870ad7fc75ca9e640
ClarkFieseln <ClarkFieseln@example.com>
2021-09-04 09:58:13 +0200

    update

--- as4pgc/AS4PGC.py
+++ as4pgc/AS4PGC.py
@@ -84,7 +84,7 @@ from sys import exit
 #       if we import it from setup.py the help shows "strange default parameters"...
 # import setup
 # from setup import __version__
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 
 # current frame
 ###############
