13b36b82c14e6cda265b519cdee1ab1227143935
Clark Fieseln <54716944+ClarkFieseln@users.noreply.github.com>
2026-05-18 18:50:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import pathlib
-from setuptools import setup
+from setuptools import setup  # , find_packages
 import sys
 
 if sys.version_info < (3,7):
-    print("as4pgc requires Python 3.7 or higher, please upgrade")
+    print("ipradar2 requires Python 3.7 or higher, please upgrade")
     sys.exit(1)
 
 # The directory containing this file
@@ -12,17 +12,16 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-__version__ = "1.5"
-# WARNING: change also as4pgc/__init__.py and as4pgc/AS4PGC.py
+__version__ = "0.3"
 
 # This call to setup() does all the work
 setup(
-    name="as4pgc",
+    name="ipradar2",
     version=__version__,
-    description = "Audio Steganography: compress, encrypt and hide a secret file inside an audio file (MP3, WAV, OGG, FLAC, OPUS, ..)",
+    description = "Intrusion Detection and Prevention in real time based e.g. on geographical locations of hosts",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/ClarkFieseln/AS4PGC",
+    url="https://www.codeproject.com/Articles/5269206/IP-Radar-2-Real-Time-Detection-and-Defense",
     author="Clark Fieseln",
     author_email="",
     license="MIT",
@@ -34,25 +33,40 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: X11 Applications :: Qt",
         "Intended Audience :: End Users/Desktop",
         "Intended Audience :: Developers",
-        "Operating System :: Microsoft :: Windows :: Windows 10",
         "Operating System :: POSIX :: Linux",
         "Topic :: Security",
     ],
-    packages=["as4pgc"],
+    packages= ["IPRadar2"],  # find_packages(),
+    package_data={
+        '.': ['IPRadar2/Icons/*.png'],
+        # '.': ['IPRadar2/Config/config.ini', 'IPRadar2/Config/locationsResolved.json'],
+    },
+    #exclude_package_data={
+    #    'Icons': ['IPRadar2/Icons/marker-dot-icon.png', 'IPRadar2/Icons/marker-icon.png'],
+    #    'img': ['IPRadar2/img/app1.jpg', 'IPRadar2/img/app2.jpg'],
+    #},
+    data_files=[
+        ('.', ['README.md']),
+        ('Icons', ['IPRadar2/Icons/marker-dot-icon.png', 'IPRadar2/Icons/marker-icon.png']),
+        ('img', ['IPRadar2/img/app1.jpg', 'IPRadar2/img/app2.jpg']),
+        ('Config', ['IPRadar2/Config/config.ini', 'IPRadar2/Config/locationsResolved.json']),
+        ('IPRadar2/Output', []),
+        ('IPRadar2/Sounds', [])
+    ],
     include_package_data=True,
-    install_requires=['soundfile','matplotlib','cryptography>=46.0.3; python_version >= "3.8"','cryptography>=39.0.0; python_version < "3.8"','numpy','scipy','bitarray','tinytag','simpleaudio'],
-    dependency_links=['https://www.ffmpeg.org/'],
-    keywords=['steganography','stego','audio','hide','cryptography','encryption','compression','security','cybersecurity','mp3','flac','ogg','opus','wav','linux'],
+    install_requires=['getmac', 'playsound', 'psutil', 'pycountry', 'sc-pylibs', 'PyQt5', 'pyshark', 'pythonping', 'Requests', 'folium', 'geographiclib', 'pygobject', 'pipreqs', 'geopy'],
+    dependency_links=['https://github.com/KimiNewt/pyshark'],
+    keywords=['cybersecurity','cyber security','cyber-security','security','IDS','IDPS','NW-IDS','NW IDS','network IDS','network-IDS','pyqt5','open streetmap','open-streetmap','network-analysis','firewall', 'pyshark', 'tshark', 'pyqt'],
     entry_points={
         "console_scripts": [
-            "as4pgc=as4pgc.AS4PGC:main",
+            "ipradar2=IPRadar2.IPRadar2:main",
         ]
     },
     project_urls={  # Optional
-    'Source': 'https://github.com/ClarkFieseln/as4pgc',
+    'Source': 'https://github.com/ClarkFieseln/IPRadar2ForLinux',
     },
 )
